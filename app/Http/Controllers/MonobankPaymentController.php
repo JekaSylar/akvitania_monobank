e2a97a9f9bd2177c8a36fef2a0659b266f4c73d6
JekaSylar <sy1ar9@hotmail.com>
2024-11-20 20:50:59 +0000

    Webhook

--- app/Http/Controllers/MonobankPaymentController.php
+++ app/Http/Controllers/MonobankPaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Models\MonobankPayment;
 
 class MonobankPaymentController extends Controller
 {
@@ -12,6 +13,32 @@ class MonobankPaymentController extends Controller
      */
     public function __invoke(Request $request)
     {
-        Log::info('Webhook payload:', $request->json()->all());
+       Log::info('Webhook payload:', $request->json()->all());
+
+        $invoiceId = $request->invoiceId;
+        $status = $request->status;
+        $amount =  (int)substr($request->amount, 0, -2);
+        $reference = $request->reference;
+        $destination = $request->destination;
+
+        $payment = MonobankPayment::where('invoiceId', $invoiceId)->first();
+        if (is_null($payment)) {
+                MonobankPayment::create([
+                    'invoiceId' => $invoiceId,
+                    'status' => $status,
+                    'amount' => $amount,
+                    'reference' => $reference,
+                    'destination' => $destination
+                ]);
+        }
+        else {
+            $payment->update([
+                'status' => $status,
+            ]);
+            if ($payment->status == 'success') {
+                Log::info('Webhook Success:');
+            }
+        }
+
     }
 }
--- database/migrations/2024_11_20_184545_create_monobank_payments_table.php
+++ database/migrations/2024_11_20_184545_create_monobank_payments_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('monobank_payments', function (Blueprint $table) {
             $table->id();
-            $table->text('x_sign')->nullable();
             $table->string('invoiceId')->unique();
             $table->string('status');
             $table->unsignedInteger('amount');
